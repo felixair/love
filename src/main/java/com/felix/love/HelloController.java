5b5cbbadac72d6021d954b639ca4e2bfeb266e5a
qin.kefu <qin.kefu@navercorp.com>
2017-09-26 16:46:44 +0800

    first page

---
+++ src/main/java/com/felix/love/HelloController.java
@@ -0,0 +1,19 @@
+package com.felix.love;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ */
+@Controller
+@RequestMapping( value = "/" )
+public class HelloController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
