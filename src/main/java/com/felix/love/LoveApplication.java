4866e41876a1e53973a75afda7d2a81927625f3b
felix <654092366@qq.com>
2017-10-09 21:47:18 +0800

    shadow mp3

--- src/main/java/com/felix/love/LoveApplication.java
+++ src/main/java/com/felix/love/LoveApplication.java
@@ -2,11 +2,27 @@ package com.felix.love;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 public class LoveApplication {
+//	public class LoveApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoveApplication.class, args);
 	}
+
+	// for export war
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(LoveApplication.class);
+//	}
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(LoveApplication.class, args);
+//	}
+
 }
